46dfbef0ee58c4f62edeca0fcc6fe221b41132cd
Manassarn Manoonchai <manassarn@gmail.com>
2023-02-28 19:11:28 +0700

    Add sidebar template

--- src/pages/overlays/index.tsx
+++ src/pages/overlays/index.tsx
@@ -3,9 +3,17 @@ import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { api } from "../../utils/api";
 import { isAdmin } from "../../utils/lib";
+import SidebarLayout from "../../components/layouts/sidebar_layout";
+import { useRouter } from "next/router";
 
 const Overlays: NextPage = () => {
-  const { data: sessionData, status } = useSession();
+  const router = useRouter();
+  const { data: sessionData, status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.replace("/api/auth/signin");
+    },
+  });
   const { data: overlays } = api.overlay.getAll.useQuery(undefined, {
     enabled: !isAdmin(sessionData),
   });
@@ -14,33 +22,35 @@ const Overlays: NextPage = () => {
     return <p>Loading...</p>;
   }
 
-  if (status === "unauthenticated") {
-    return <p>Access Denied</p>;
-  }
-
   if (!isAdmin(sessionData)) {
     return <p>Access Denied</p>;
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <h1>Overlays</h1>
-      <ul className="list-disc">
-        {overlays &&
-          overlays.map((overlay) => {
-            return (
-              <li key={overlay.id}>
-                <Link href={`/overlays/${overlay.id}`} className="underline">
-                  {overlay.name}
-                </Link>
-              </li>
-            );
-          })}
-      </ul>
-      <Link href="/" className="underline">
-        Home
-      </Link>
-    </main>
+    <SidebarLayout>
+      <div className="py-6">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <h1 className="text-2xl font-semibold text-gray-900">Overlays</h1>
+        </div>
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <ul className="list-disc">
+            {overlays &&
+              overlays.map((overlay) => {
+                return (
+                  <li key={overlay.id}>
+                    <Link
+                      href={`/overlays/${overlay.id}`}
+                      className="underline"
+                    >
+                      {overlay.name}
+                    </Link>
+                  </li>
+                );
+              })}
+          </ul>
+        </div>
+      </div>
+    </SidebarLayout>
   );
 };
 
